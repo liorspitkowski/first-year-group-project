bc02ed58c2f1f1934ddfb943baa7c8c8f1cb1a04
a24872zh <ziggy.hughes@student.manchester.ac.uk>
2022-02-09 17:30:53 +0000

    added to connection

--- test_connection.php
+++ test_connection.php
@@ -4,7 +4,7 @@ $database_user = "a24872zh";
 $database_pass = "12345x18";
 $database_name = "a24872zh";
 
-$conn = mysqli_connect($database_host, $database_user, $database_pass);
+$conn = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
 
 if (!$conn) {
   die("CONNECTION FAILED: " . mysqli_connect_error());
