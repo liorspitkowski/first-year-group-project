31c32762f9977b6c7a7ec64237c42c021e580da7
a24872zh <ziggy.hughes@student.manchester.ac.uk>
2022-02-09 17:18:42 +0000

    changed password

--- test_connection.php
+++ test_connection.php
@@ -1,7 +1,7 @@
 <?php
 $database_host = "dbhost.cs.man.ac.uk";
 $database_user = "a24872zh";
-$database_pass = "Lymington10";
+$database_pass = "12345x18";
 $database_name = "a24872zh";
 
 $conn = mysqli_connect($database_host, $database_user, $database_pass);
