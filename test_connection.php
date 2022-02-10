fdd2e6a741a9b3468d11136fcb8a4bdb6e465415
a24872zh <ziggy.hughes@student.manchester.ac.uk>
2022-02-10 16:13:04 +0000

    added php file

--- test_connection.php
+++ test_connection.php
@@ -4,6 +4,11 @@ $database_user = "a24872zh";
 $database_pass = "12345x18";
 $database_name = "a24872zh";
 
+
+// require_once('config.inc.php'); 
+
+// merge then add to publish webpage
+// if this doesnt work try group database instead
 $conn = mysqli_connect($database_host, $database_user, $database_pass, $database_name);
 
 if (!$conn) {
