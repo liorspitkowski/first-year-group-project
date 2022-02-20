1d3fa665ac77b84cb830adc1cb2c58c61b3332a3
k47680dm <daniel.makin@student.manchester.ac.uk>
2022-02-20 23:29:18 +0000

    Basic Files written to Create shopping list and Display Inventory

--- creatingShoppingList.php
+++ creatingShoppingList.php
@@ -1,7 +1,31 @@
 <?php
 	require "DatabaseHandler.php";
-	$conn = connect(true);
-	$conn = connect();
 
-	
+	function displayInventory(){
+		$conn = connect(true);
+		$sql = "SELECT " 
+	}
+
+	function getFoodId($conn, $ingredient){
+
+	  $sql = "SELECT foodId FROM foods WHERE foodName = :ingredient";
+	  $stmt = $conn->prepare($sql);
+
+	  $stmt->execute([
+	    'ingredient' => $ingredient
+	  ]);
+	  return $stmt;
+	}
+
+	function getFoodIdFromRecipe($conn, $recipeId){
+		$sql = "SELECT food_id, food_id FROM ingredients WHERE recipe_id = :recipeId";
+		$stmt = $conn->prepare($sql);
+
+	  $stmt->execute([
+	    'ingredient' => $ingredient
+	  ]);
+	  return $stmt;
+
+	  //then increment the data in the calling function after searching for each element
+	}
 ?>
---
+++ displayInventory.php
@@ -0,0 +1,44 @@
+<?php
+//change names to db proper ones
+	require "DatabaseHandler.php";
+
+	function displayInventory(){
+		$conn = connect(true);
+
+	}
+
+	function getFoodId($conn, $ingredient){
+
+	  $sql = "SELECT foodName FROM foods WHERE foodId = :ingredient";
+	  $stmt = $conn->prepare($sql);
+
+	  //change this
+	  $stmt->execute([
+	    'ingredient' => $ingredient
+	  ]);
+	  return $stmt;
+	}
+
+	function getInventoryFoodNames($conn, $userId){
+		//$sql = "SELECT food_id, quantity FROM recipes WHERE user_id = :userId";
+		$sql = "SELECT food_name FROM food WHERE food_id = (SELECT food_id FROM Inventory WHERE user_id = :userId)";
+		$stmt = $conn->prepare($sql);
+
+		$stmt->execute([
+		  'userId' => $userId
+		]);
+
+		return $stmt;
+	}
+
+	function getQuantities($conn, $userId){
+		$sql = "SELECT quantity FROM Inventory WHERE user_id = :userId";
+		$stmt = $conn->prepare($sql);
+
+		$stmt->execute([
+		  'userId' => $userId
+		]);
+
+		return $stmt;
+	}
+?>
