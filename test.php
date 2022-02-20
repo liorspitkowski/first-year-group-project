d87d6fe65030a932b598fd0b44872998b6707ade
dlharry <dlharry@126.com>
2022-02-20 17:26:35 +0000

    fixed local apache and test again - pass

--- test.php
+++ test.php
@@ -1,23 +1,12 @@
-<!DOCTYPE html>
-<html>
-
-<head>
-	<meta charset="utf-8">
-	<title>Test Project</title>
-
-	<?php
-
-
-	echo 'Hello ';
-
-
-	?>
-
-
-</head>
-
-<body>
-
-</body>
-
-</html>
+<?php
+if ($_SERVER["REQUEST_METHOD"] == "POST") {
+  // collect value of input field
+  $name = $_POST['user_name'];
+  $password = $_POST['user_password'];
+  if (empty($name)) {
+    echo "Name is empty";
+  } else {
+    echo 'Entered name is :' . $name . '<br>password is :' . $password;
+  }
+}
+?>
