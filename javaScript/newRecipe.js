d28810914318bae2161795a7333284d7b886d13a
leittb <34034602+leittb@users.noreply.github.com>
2022-02-25 10:54:04 +0000

    can now scroll through autofill items with arrows

--- javaScript/newRecipe.js
+++ javaScript/newRecipe.js
@@ -17,7 +17,7 @@ function addIngredientFeild(){
   input1.id = "ingredient" + lenTable;
   input1.placeholder = "food";
   input1.autocomplete = "off";
-  input1.addEventListener('input', autoComplete);
+  autoCompleteFood(input1);
   var input2 = document.createElement("input");
   input2.required = true;
   input2.type = "number";
@@ -63,75 +63,120 @@ function submitRecipe(){
    });
 }
 
-const autoComplete = function autoCompleteFood(e){
+function autoCompleteFood(input){
 
-  document.addEventListener("click", function (e) {
-      closeLists();
+  var focus = -1;
+
+  /*adds event listener to send a request to the database when a letter is entered or removed
+  from the input box*/
+  input.addEventListener('input', function(e){
+    var url = "../PHP/GetIngridientParts.php",
+    input = e.target;
+    inputVal = e.target.value;
+    $.ajax({
+        url: url,
+        type: 'post',
+        data: {input: inputVal, function: "autofill"},
+        success: function(data)
+         {
+           closeLists();
+           if (inputVal.length < 1) return;
+           var foods = data.split(",");
+           if (foods[0] == "") return;
+           list = document.createElement("DIV");
+           list.setAttribute("id", e.target.id + "-autocomplete-list");
+           list.setAttribute("class", "autocomplete-items");
+           e.target.parentNode.appendChild(list);
+
+           console.log(foods);
+
+           for (i = 0; i < foods.length && i <= 5; i++){
+             item = document.createElement("DIV");
+             item.innerHTML = "<strong>" + foods[i].substr(0, inputVal.length) + "</strong>";
+             item.innerHTML += foods[i].substr(inputVal.length);
+             item.innerHTML += "<input type='hidden' value='" + foods[i] + "'>";
+             item.addEventListener("click", function(event){
+               //autofills value clicked by user
+               const food = this.getElementsByTagName("input")[0].value;
+               input.value = food;
+               //gets default units for the selected food
+               $.ajax({
+                   url: "../PHP/GetIngridientParts.php",
+                   type: 'post',
+                   data: {input: food, function: "defaultUnits"},
+                   success: function(unitVals) {
+                     var units = unitVals.split(",");
+                     if (units.length == 1){
+                       input.parentNode.parentNode.getElementsByClassName("unit")[0].value = units[0];
+                     }
+                     else{
+                       console.log(units);
+                     }
+                   }
+               });
+               closeLists();
+             });
+             list.appendChild(item);
+           }
+         }
+     });
   });
 
-  var url = "../PHP/GetIngridientParts.php",
-  input = e.target;
-  inputVal = e.target.value;
-  $.ajax({
-      url: url,
-      type: 'post',
-      data: {input: inputVal, function: "autofill"},
-      success: function(data)
-       {
-         closeLists();
-         if(inputVal.length < 1){return;}
-         var foods = data.split(",");
-         list = document.createElement("DIV");
-         list.setAttribute("id", e.target.id + "-autocomplete-list");
-         list.setAttribute("class", "autocomplete-items");
-         e.target.parentNode.appendChild(list);
+  /*adds event listener to allow scrolling through aoutfill options with arrow keys */
+  input.addEventListener('keydown', function(e){
+    var list = document.getElementById(this.id + "-autocomplete-list");
+    if (list) list = list.getElementsByTagName("div");
 
-         for (i = 0; i < foods.length && i <= 5; i++){
-           item = document.createElement("DIV");
-           item.innerHTML = "<strong>" + foods[i].substr(0, inputVal.length) + "</strong>";
-           item.innerHTML += foods[i].substr(inputVal.length);
-           item.innerHTML += "<input type='hidden' value='" + foods[i] + "'>";
-           item.addEventListener("click", function(event){
-             //autofills value clicked by user
-             const food = this.getElementsByTagName("input")[0].value;
-             input.value = food;
-             //gets default units for the selected food
-             $.ajax({
-                 url: "../PHP/GetIngridientParts.php",
-                 type: 'post',
-                 data: {input: food, function: "defaultUnits"},
-                 success: function(unitVals) {
-                   var units = unitVals.split(",");
-                   if (units.length == 1){
-                     input.parentNode.parentNode.getElementsByClassName("unit")[0].value = units[0];
-                   }
-                   else{
-                     console.log(units);
-                   }
-                 }
-             });
-             closeLists();
-           });
-           list.appendChild(item);
-         }
-       }
-   });
+    if (e.keyCode == 40) {
+      focus++;
+      addActive(list);
+    }
+    if (e.keyCode == 38) {
+      focus--;
+      addActive(list);
+    }
+    if (e.keyCode == 13) {
+      e.preventDefault();
+        if (focus > -1) {
+          /*simulate a click on the "active" item:*/
+          if (list) list[focus].click();
+        }
+    }
+  });
 
+  function addActive(list) {
+    if (!list){
+      return false;
+    }
+    removeActive(list);
+    if (focus >= list.length) focus = 0;
+    if (focus < 0) focus = (list.length - 1);
+    list[focus].classList.add("autocomplete-active");
+  }
+
+  function removeActive(list) {
+    for (var i = 0; i < list.length; i++) {
+      list[i].classList.remove("autocomplete-active");
+    }
+  }
 
    function closeLists(){
-     var x = document.getElementsByClassName("autocomplete-items");
-     for (var i = 0; i < x.length; i++) {
-       x[i].parentNode.removeChild(x[i]);
+     var list = document.getElementsByClassName("autocomplete-items");
+     for (var i = 0; i < list.length; i++) {
+       list[i].parentNode.removeChild(list[i]);
      }
    }
+
+   document.addEventListener("click", function (e) {
+       closeLists();
+   });
 
 }
 
 function init(){
+
   input = document.getElementById("ingredient1");
-  if (input){
-    input.addEventListener('input', autoComplete);
-  }
+  autoCompleteFood(input);
 
   addIngredientFeild();
   addIngredientFeild();
