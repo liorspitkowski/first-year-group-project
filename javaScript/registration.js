defa0861ba68249abe0d1035cc6dd8c0572ad27f
dlharry <dlharry@126.com>
2022-02-26 16:32:42 +0000

    update registration js function

--- javaScript/registration.js
+++ javaScript/registration.js
@@ -1,3 +1,31 @@
-function submitReg(){
-    
+function submitReg() {
+    alert('function called');
+    var url = "../PHP/register.php", data = $('#registration_form').serialize();
+    console.log(data);
+    $.ajax({
+        // prevent page reload, dunno the reason
+        async: false,
+        url: url,
+        type: 'POST',
+        data: data,
+        /*
+            return:
+            if user already exists: 0
+            if register successful: 1
+        */
+        success: function (data) {
+            //alert(data);  show response from the php script.
+            if (data == "0") {
+                alert('user already exists\nplease go to login page');
+            }
+            else if ( data == "1") {
+                alert('welcome back to Foogle');
+            }
+            else {
+                alert('server response invalid value: ' + data);
+            }
+        }
+    });
+    // prevent page reload, dunno the reason
+    return false;
 }
