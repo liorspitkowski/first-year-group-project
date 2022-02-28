6a8387aa8735a3e7c035844a73527c64b1463f92
Yuhan Wang <yuhan.wang-9@student.manchester.ac.uk>
2022-02-28 01:26:59 +0000

    update shopplist and samplerecipe.css.html.js

---
+++ javaScript/samplerecipe.js
@@ -0,0 +1,26 @@
+function submitSample() {
+    alert('function called');
+    var url = "#", data = $('samole_form').serialize();
+    console.log(data);
+    $.ajax({
+        // prevent page reload, dunno the reason
+        async:false,
+        url: url,
+        type: 'POST',
+        data: data,
+        success: function (data) {
+            //alert(data);  show response from the php script.
+            if(data == "0" | data =="1"){
+                alert('username or password incorrect, \nplease check again');
+            }
+            else if(data == "2"){
+                alert('welcome back to Foogle');
+            }
+            else{
+                alert('server response invalid value: ' + data);
+            }
+        }
+    });
+    // prevent page reload, dunno the reason
+    return false;
+}
