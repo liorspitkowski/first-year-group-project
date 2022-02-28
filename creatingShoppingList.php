5df57fbbffa2850e37ba2d9b739f28c87c3862db
k47680dm <daniel.makin@student.manchester.ac.uk>
2022-02-28 12:23:30 +0000

    Comparison Lists working

--- creatingShoppingList.php
+++ creatingShoppingList.php
@@ -1,6 +1,6 @@
 <?php
 	require "DatabaseHandler.php";
-
+	// $user = $_POST['userId'];
 	mainFunction(1);
 
 	function getRecipeId($conn, $recipeName){
@@ -46,19 +46,59 @@
 		return array($Ids, $amount);
 	}
 	function compareLists($conn, $userId, $ingredients){
+		for ($i = 0; $i < count($ingredients); $i++){
+			//try to select the ingredient in the inventory list
+			$sql = "SELECT amount FROM inventory WHERE userId = :userId AND foodId = :foodId";
+			$stmt = $conn->prepare($sql);
+			$stmt->execute(['userId' => $userId, 'foodId' => $ingredients[0][$i]]);
+			//checks if the user has any
+			if ($stmt == true){
+				$ingredients[1][$i] -= ($stmt->fetch()['amount']);
+			}
 
+		}
+		$newAmounts = [];
+		$newIngredients = [];
+
+
+		for ($i = 0; $i < count($ingredients[0]); $i++){
+			//check each element whether it is below or equal to 0
+			if ($ingredients[1][$i] > 0){
+				array_push($newAmounts, $ingredients[1][$i]);
+				array_push($newIngredients, $ingredients[0][$i]);
+			}
+		}
+
+		//then replace the new array
+
+		return array($newIngredients, $newAmounts);
 	}
+	function getIngredientNames($conn, $Ids){
+		$arrayNames = [];
+		for ($i = 0; $i < count($Ids); $i++){
+			$sql = "SELECT foodName FROM foods WHERE foodId = :foodId";
+			$stmt = $conn->prepare($sql);
 
+			$stmt->execute([
+				'foodId' => $Ids[$i]
+			]);
 
+			if ($stmt != null){
+				while ($row = $stmt->fetch()){
+					array_push($arrayNames, $row['foodName']);
+				}
+			}
+		}
+		return $arrayNames;
+	}
 	function mainFunction($user){
 		$conn = connect(true);
 
-		$recipeId = getRecipeId($conn, "beans on toast");
-		//$inventory = getInventory($conn, $user);
+		$recipeId = getRecipeId($conn, "Chicken Korma");
 		$ingredients = getIngredients($conn, $user, $recipeId);
 
-		$finalList = compareLists($conn, $userId, $ingredients)
+		$finalList = compareLists($conn, $user, $ingredients);
+		$finalList[0] = getIngredientNames($conn, $finalList[0]);
+		var_dump($finalList);
 	}
-
-
 ?>
