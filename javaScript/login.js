b7f68fe9a6566e605385fcdeae01b590eb1a014d
dlharry <dlharry@126.com>
2022-03-02 12:21:34 +0000

    update login js

--- javaScript/login.js
+++ javaScript/login.js
@@ -9,19 +9,24 @@ function submitLogin() {
         type: 'POST',
         data: data,
         success: function (data) {
+            let flag = getValue("flag", data);
+            let userid = getValue("userid", data);
+            alert("flag= "+flag);
+            alert("userid= "+userid);
             //alert(data);  show response from the php script.
-            if (data == "0" | data == "1") {
+            if (flag == "0" | flag == "1") {
                 alert('username or password incorrect, \nplease check again');
             }
-            else if (data[0] == "2") {
-                alert('welcome back to Foogle' + data[1]);
+            else if (flag == "2") {
+                //flag=2;username=sjdf;
+                alert('welcome back to Foogle ' + userid);
                 for (var pair of formData.entries()) {
                     console.log(pair[0] + ': ' + pair[1]);
                     if(pair[0] == 'user_name'){
                         document.cookie = 'username=' + pair[1] + '; expires=18 Dec 2025 12:00:00 UTC;path=/';
                     }
                 }
-                document.cookie = 'userid=' + data[1] + '; expires=18 Dec 2025 12:00:00 UTC;path=/';
+                document.cookie = 'userid=' + userid + '; expires=18 Dec 2025 12:00:00 UTC;path=/';
                 location.href = "../html/menu.html";
             }
             else {
