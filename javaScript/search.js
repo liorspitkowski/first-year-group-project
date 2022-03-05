21ec2f0ea3d7110a45cdff396250c84ef38bdf6b
dlharry <dlharry@126.com>
2022-03-05 12:37:34 +0000

    update search display function

--- javaScript/search.js
+++ javaScript/search.js
@@ -2,22 +2,117 @@
     Written by: Hanmin Liu;
     send search form, expect a string to be displayed in the search bar.
 */
+var page_num/* total pages num */, current_page/* current_page/page_num */, recipe_array/* results string array */, max_show = 5/* max elements per page */;
 function submitSearch() {
-    alert('function called');
     // console.log(document.querySelector('#search_form'));
     var url = "../PHP/SearchRecipe.php", data = $('#search_form').serialize();
     console.log(data);
     $.ajax({
-        async:false,
+        async: false,
         url: url,
         type: 'POST',
         data: data,
         success: function (data) {
             console.log(data);
             // 16 beans on toast;fancy beans on toast;beans in toast;beanz;beanz+;test recipe;Chicken Korma
-            let recipename = getValue_noName(data);
-            console.log(recipename);
+            // initiate recipe_array, page_num, current_page
+            recipe_array = getValue_noName(data);
+            // let totalresult = recipe_array.length;
+            recipe_array = listToMatrix(recipe_array, max_show);
+            // Get how many pages
+            page_num = recipe_array.length;
+            /* DEBUG START */
+            // console.log("num=" + totalresult);
+            // let totalpage = Math.ceil(totalresult / max_show);
+            // console.log("totalpage=" + page_num);
+            console.log(recipe_array);
+            /* DEBUG END */
+            current_page = 1;
+            for (let i = 1; i <= max_show; i++) {
+                // write recipes;
+                addElement(i, "button", "result-" + i, recipe_array[current_page - 1][i - 1], "search-results");
+                // write index;
+            }
         }
     });
     return false;
+}
+/* create search results */
+function addElement(
+    id, // unique identifier in the page
+    divtype, // this case, button.
+    newdivname, //the created div name
+    content, // the content inside
+    parentdiv // where to insert
+) {
+    let newDiv = document.createElement(divtype);
+    newDiv.name = newdivname;
+    newDiv.onclick = function () {
+        console.log("this is the page " + current_page + " number " + id);
+        console.log("recipe name is:" + content);
+    };
+    let newContent = document.createTextNode(content);
+    newDiv.appendChild(newContent);
+    let currentDiv = document.getElementById(parentdiv);
+    currentDiv.appendChild(newDiv);
+}
+
+/* delete current results */
+function delElement(deldivname, parentdiv) {
+    // error: not 'node'
+    console.log("deleting "+deldivname);
+    var d = document.getElementsByName(deldivname);
+    console.log("d is "+d);
+    for (var i = 0; i < d.length; ++i) {
+        var item = d[i];  // 调用 myNodeList.item(i) 是没有必要的
+        console.log(item);
+        document.getElementById(parentdiv).removeChild(item);
+    }
+    
+}
+
+/* convert list to n elements per row. */
+function listToMatrix(list, elementsPerSubArray) {
+    var matrix = [], i, k;
+    for (i = 0, k = -1; i < list.length; i++) {
+        if (i % elementsPerSubArray === 0) {
+            k++;
+            matrix[k] = [];
+        }
+        matrix[k].push(list[i]);
+    }
+    return matrix;
+}
+function nextPage() {
+    if (current_page + 1 > page_num) {
+        console.log("end of pages");
+        return false;
+    }
+    clearPage();
+    current_page += 1;
+    for (let i = 1; i <= max_show; i++) {
+        // write recipes;
+        addElement(i, "button", "result-" + i, recipe_array[current_page - 1][i - 1], "search-results");
+        // write index;
+    }
+}
+function prevPage() {
+    if (current_page - 1 <= 0) {
+        console.log("head of pages");
+        return false;
+    }
+    clearPage();
+    current_page -= 1;
+    for (let i = 1; i <= max_show; i++) {
+        // write recipes;
+        addElement(i, "button", "result-" + i, recipe_array[current_page - 1][i - 1], "search-results");
+        // write index;
+    }
+}
+function clearPage() {
+    for (let i = 1; i <= max_show; i++) {
+        // delete recipes;
+        delElement("result-" + i, "search-results");
+        // write index;
+    }
 }
