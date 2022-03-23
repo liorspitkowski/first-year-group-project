bcaa137f3f65e606aa0cfb6f3eb2cb3b7b127777
a24872zh <ziggy.hughes@student.manchester.ac.uk>
2022-03-23 17:32:03 +0000

    send recipe id

--- javaScript/editShoppingList.js
+++ javaScript/editShoppingList.js
@@ -1,8 +1,11 @@
 function addToSL() {
   alert('function call');
   let user_id = getCookie('userid');
+  let recipe_id = document.getElementById('recipe_id')
   alert(user_id);
+  alert(recipe_id);
   $('#add_to_list').append('<input type="hidden" name="user_id" value="'+user_id+'" /> ');
+  $('#add_to_list').append('<input type="hidden" name="recipe_id" value="'+recipe_id+'" /> ');
   var url = "../PHP/addRecipeToList.php";
   var data = $('#add_to_list').serialize();
   console.log(data);
@@ -22,8 +25,11 @@ function addToSL() {
 function removeFromSL() {
   alert('function call');
   let user_id = getCookie('userid');
+  let recipe_id = document.getElementById('recipe_id')
   alert(user_id);
+  alert(recipe_id);
   $('#remove_from_list').append('<input type="hidden" name="user_id" value="'+user_id+'" /> ');
+  $('#remove_from_list').append('<input type="hidden" name="recipe_id" value="'+recipe_id+'" /> ');
   var url = "../PHP/removeRecipeFromList.php", data = $('#remove_from_list').serialize();
   console.log(data);
   alert(data);
