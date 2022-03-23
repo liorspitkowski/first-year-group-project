342d2d7e28fc7ad25b3085097754c0c6bab6e552
k47680dm <daniel.makin@student.manchester.ac.uk>
2022-03-23 17:39:45 +0000

    display recipe page written and tested

---
+++ PHP/displayRecipesOnList.php
@@ -0,0 +1,59 @@
+<?php
+	//Written and Maintained by Daniel Makin
+
+	require "DatabaseHandler.php";
+	//get parameters
+	$userId = $_POST['userId'];
+	
+	mainFunction($userId);
+
+
+	function mainFunction($userId){
+		//establish connection
+		$conn = connect(true);
+		$recipes = getRecipeInfo($conn, $userId);
+		//gets the recipe names associated with the ids
+		$recipes = getRecipeNames($conn, $recipes[0], $recipes[1]);
+		echo(returnData($recipes[0], $recipes[1]));
+	}
+
+	function getRecipeNames($conn, $Ids, $portions){
+		$names = [];
+		for ($i = 0; $i < count($Ids); $i++){
+			$sql = "SELECT recipeName FROM recipes WHERE recipeId = :recipe";
+			$stmt = $conn->prepare($sql);
+			$stmt->execute(['recipe' => $Ids[$i]]);
+			//will always return a value
+			array_push($names, $stmt->fetch()['recipeName']);
+		}
+
+		return array($names, $portions);
+	}
+
+	function returnData($names, $portions){
+		$endString = "";
+		for ($i = 0; $i < (count($names) - 1); $i++){
+			$endString = $endString . $names[$i] . "#" . $portions[$i] . "#";
+		}
+		$endString = $endString . $names[count($names) - 1] . "#" . $portions[count($names) - 1];
+		return $endString;
+	}
+
+
+	function getRecipeInfo($conn, $userId){
+		$Ids = [];
+		$portions = [];
+		$sql = "SELECT recipeId, portions FROM shopRecipes WHERE userId = :user";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute(['user' => $userId]);
+
+		while ($row = $stmt->fetch()){
+			array_push($Ids, $row['recipeId']);
+			array_push($portions, $row['portions']);
+		}
+
+		return array($Ids, $portions);
+	}	
+
+
+?>
