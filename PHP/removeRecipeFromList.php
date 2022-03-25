9035e4dbbe21f3092ba0a6cc56d96ab22544cb78
k47680dm <daniel.makin@student.manchester.ac.uk>
2022-03-25 13:34:01 +0000

    Bug Fixes

--- PHP/removeRecipeFromList.php
+++ PHP/removeRecipeFromList.php
@@ -3,14 +3,20 @@
 	
 	require "DatabaseHandler.php";
 
-	$userId = $_POST['userId'];
-	$recipeId = $_POST['recipeId'];
+	$userId = $_POST['user_id'];
+	$recipeId = $_POST['recipe_id'];
 
 	mainFunction($userId, $recipeId);
 
 	function mainFunction($userId, $recipeId){
 		//removes the item from the table
 		$conn = connect(true);
+		//check record exists
+		if (checkIfInDatbase() == false){
+			return "flag=1";
+		}
+
+
 		removeRecord($conn, $userId, $recipeId);
 
 		echo(checkRecordRemoved($conn, $userId, $recipeId));
@@ -22,7 +28,17 @@
 		$stmt->execute(['user' => $userId, 'recipe' => $recipeId]);
 	}
 
+	function checkIfInDatbase($conn, $userId, $recipeId){
+		$sql = "SELECT * FROM recipeList WHERE userId = :user AND recipeId = :recipe";
+		$stmt = $conn->prepare($sql);
+		$stmt->execute(['user' => $userId, 'recipe' => $recipeId])
+
+		while ($row = $stmt->fetch()){
+			return true;
+		}
+		return false;
 	}
+
 	function checkRecordRemoved($conn, $userId, $recipeId){
 		$sql = "SELECT * FROM shopRecipes WHERE userId = :user AND recipeId = :recipe";
 		$stmt = $conn->prepare($sql);
@@ -33,7 +49,7 @@
 			return "flag=0";
 		}
 		//this means nothing was returned
-		return "flag=1";
+		return "flag=2";
 	}
 
 ?>
