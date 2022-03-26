3d8bb9af004e2f6275d9ff3bb4e9333fac76866d
dlharry <dlharry@126.com>
2022-03-26 19:42:18 +0000

    update index style

--- javaScript/index.js
+++ javaScript/index.js
@@ -2,48 +2,50 @@
     Written by Hanmin Liu;
     when user enter the index page, check if the user has a cookie, and force logged in as a guest user if not;
     Allow users log out. By setting cookie to guestuser
-*/ 
+*/
 function checkIndexUser() {
     let user_name = getCookie('username');
     let user_id = getCookie('userid');
     console.log(user_name + ' ' + user_id);
-    if(user_id == '' || user_name == ''){
+    if (user_id == '' || user_name == '') {
         initializeGuest();
         document.getElementById('currentuser').innerHTML = "Welcome back! Guest user.";
-    }
-    else if (user_id == '1') {
+    } else if (user_id == '1') {
         document.getElementById('currentuser').innerHTML = "Welcome back! Guest user.";
-    }
-    else {
+    } else {
         let MorG = document.getElementById('changeButton');
         changeGuest(MorG);
         addLogoutBu(user_name);
     }
 }
+
 function changeGuest(button) {
-    button.innerHTML = 'Menu';
-    button.href = './html/menu.html';
+    document.getElementById('changeButton_p').innerHTML = 'Menu';
 }
+
 function addLogoutBu(username) {
     let parentd = document.getElementById('currentuser');
     parentd.innerHTML = "Welcome back! " + username + ".";
     let button = document.createElement('button');
     button.id = 'logout';
     button.innerHTML = "Logout";
-    button.addEventListener('click',function(e){
+    button.addEventListener('click', function(e) {
         e.preventDefault();
         GuestLogin();
         window.location.reload();
     });
     parentd.appendChild(button);
 }
-function GuestLogin(){
+
+function GuestLogin() {
     setCookieasGuest();
 }
-function initializeGuest(){
+
+function initializeGuest() {
     setCookieasGuest();
 }
-function setCookieasGuest(){
+
+function setCookieasGuest() {
     console.log("setting guestuser");
     document.cookie = "username=guestUser; expires=18 Dec 2025 12:00:00 UTC; path=/;";
     document.cookie = "userid=1; expires=18 Dec 2025 12:00:00 UTC; path=/;";
