ce8d2f675c4318c04dd377a6ba6012f1364b0299
dlharry <dlharry@126.com>
2022-03-29 18:48:11 +0100

    Update Shopping list

--- javaScript/getCookie.js
+++ javaScript/getCookie.js
@@ -5,17 +5,18 @@
     let x = getCookie('username');
 */
 function getCookie(cname) {
+    cname = String(cname);
     let name = cname + "=";
     let decodedCookie = decodeURIComponent(document.cookie);
     let ca = decodedCookie.split(';');
     for (let i = 0; i < ca.length; i++) {
-      let c = ca[i];
-      while (c.charAt(0) == ' ') {
-        c = c.substring(1);
-      }
-      if (c.indexOf(name) == 0) {
-        return c.substring(name.length, c.length);
-      }
+        let c = ca[i];
+        while (c.charAt(0) == ' ') {
+            c = c.substring(1);
+        }
+        if (c.indexOf(name) == 0) {
+            return c.substring(name.length, c.length);
+        }
     }
     return "";
-  }
+}
